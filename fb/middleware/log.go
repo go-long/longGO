f22a8f4ccec01fec211df0d21df1aee7870cf900
Justin.h <huangqiaojia@letv.com>
2016-04-24 17:20:42 +0800

    add route filter blacklist

--- fb/middleware/log.go
+++ fb/middleware/log.go
@@ -3,18 +3,41 @@ package middleware
 import (
 	"net"
 	"time"
-
+"strings"
 	"github.com/labstack/echo"
 	"github.com/labstack/gommon/color"
 	"github.com/labstack/echo/engine/standard"
 )
 
+var LogBlacklist =&TBlacklist{}
+
+type (
+	TBlacklist []blackNote
+
+	blackNote struct {
+	  Method string
+          Prefix string
+        }
+)
+
 func Log() echo.MiddlewareFunc {
 	return func(h echo.HandlerFunc) echo.HandlerFunc {
 		return  func(c echo.Context) error {
+
 			req := c.Request()
 			res := c.Response()
+
 			logger := c.Logger()
+			method := req.Method()
+			path := req.(*standard.Request).RequestURI
+			if path == "" {
+				path = "/"
+			}
+                        //黑名单不打印日志
+			if LogBlacklist.Exists(method,path) {
+				//return nil
+				return h(c)
+			}
 
 			remoteAddr := req.RemoteAddress()
 			if ip := req.Header().Get(echo.HeaderXRealIP); ip != "" {
@@ -30,11 +53,7 @@ func Log() echo.MiddlewareFunc {
 				c.Error(err)
 			}
 			stop := time.Now()
-			method := req.Method()
-			path := req.(*standard.Request).RequestURI
-			if path == "" {
-				path = "/"
-			}
+
 
 			size := res.Size()
 
@@ -53,4 +72,17 @@ func Log() echo.MiddlewareFunc {
 			return nil
 		}
 	}
+}
+
+func (b *TBlacklist)Add(method,url string){
+	*b=append(*b,blackNote{method,url})
+}
+
+func (b *TBlacklist)Exists(method,url string) bool{
+	for _,v:=range *b{
+		if v.Method==method && strings.HasPrefix(url, v.Prefix){
+			return true
+		}
+	}
+	return false
 }
